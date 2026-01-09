00d5bf9595038188545fe57bbd8d49f478b90a0c
tonmoy-Org <tonmoy15-5200@diu.edu.bd>
2026-01-09 19:44:30 +0600

    Add model weights with Git LFS

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: 'https://sterling-dashboard-eta.vercel.app' ,
+    origin: 'https://sterling-dashboard-snowy.vercel.app' ,
     credentials: true,
   })
 );
